dda464ef2668bcd1602fa9f566b0294f788d2e1d
AatirNadim <aatir.nadim@gmail.com>
2025-08-19 23:13:51 +0530

    feat: add layout and page components for Admin and Superadmin Consoles

---
+++ apps/ui/administration-ui/app/[tenant]/admin/console/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Console",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>{children}</body>
+    </html>
+  );
+}
---
+++ apps/ui/administration-ui/app/[tenant]/superadmin/console/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>Superadmin Console</div>;
+};
+
+export default page;
