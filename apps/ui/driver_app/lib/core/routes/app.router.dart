f457a5264417f61d87c105acfa9539ed9280e893
AatirNadim <aatir.nadim@gmail.com>
2025-08-29 00:16:42 +0530

    feat: add HomeScreen widget and update app router to use const constructor

--- apps/ui/driver_app/lib/core/routes/app.router.dart
+++ apps/ui/driver_app/lib/core/routes/app.router.dart
@@ -1,5 +1,6 @@
 import "package:go_router/go_router.dart";
+import "package:driver_app/screens/home.screen.dart";
 
 final appRouter = GoRouter(
-  routes: [GoRoute(path: '/', builder: (context, state) => HomeScreen())],
+  routes: [GoRoute(path: '/', builder: (context, state) => const HomeScreen())],
 );
