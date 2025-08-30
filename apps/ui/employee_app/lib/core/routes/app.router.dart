731fde17a8481c8699c963ae35267cd84bf75ba8
AatirNadim <aatir.nadim@gmail.com>
2025-08-30 19:28:24 +0530

    feat: init flutter based platform-agnostic employee app

---
+++ apps/ui/employee_app/lib/core/routes/app.router.dart
@@ -0,0 +1,10 @@
+// import "package:driver_app/screens/login/login.screen.dart";
+import "package:go_router/go_router.dart";
+// import "package:driver_app/screens/home.screen.dart";
+
+final appRouter = GoRouter(
+  routes: [
+    // GoRoute(path: '/', builder: (context, state) => const HomeScreen()),
+    // GoRoute(path: '/login', builder: (context, state) => const LoginScreen()),
+  ],
+);
