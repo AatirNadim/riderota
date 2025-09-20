4246744ab69fb3b0d889a6d886644fb43d8fa251
AatirNadim <aatir.nadim@gmail.com>
2025-09-21 00:59:20 +0530

    feat: refactor ApiClient to improve token management and interceptor structure

--- apps/ui/driver_app/lib/api/api-client.dart
+++ apps/ui/driver_app/lib/api/api-client.dart
@@ -4,48 +4,64 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:driver_app/core/storage/secure-storage.dart';
 
 class ApiClient {
-  Dio _dio;
+  late final Dio _dio;
   late final SecureStorageService _secureStorageService;
 
   ApiClient(){
     
     _secureStorageService = getIt<SecureStorageService>();
 
-    // List<String?> tokens = Future.wait([
-    //   _secureStorageService.getAccessToken(),
-    //   _secureStorageService.getRefreshToken(),
-    // ]) as List<String?>;
   
     _dio = Dio(
         BaseOptions(
           baseUrl: dotenv.env['API_BASE_URL'] ?? 'https://yourapi.com',
           connectTimeout: const Duration(seconds: 15),
           receiveTimeout: const Duration(seconds: 15),
-          // headers: {
-          //   if (tokens[0] != null) 'Authorization': 'Bearer ${tokens[0]}',
-          //   if (tokens[1] != null) 'x-refresh-token': tokens[1] ?? '',
-          // },
         ),
       );
-    _dio.interceptors.add(
-      LogInterceptor(responseBody: true), // For debugging
-      // Your custom AuthInterceptor will go here
-      // add tokens to each outgoing request if they exist
-      InterceptorsWrapper(
-        onRequest: 
-        // onRequest: (options, handler) async {
-        //   List<String?> tokens = await Future.wait([
-        //     _secureStorageService.getAccessToken(),
-        //     _secureStorageService.getRefreshToken(),
-        //   ]) as List<String?>;
+    _dio.interceptors.addAll(<Interceptor>[
+      LogInterceptor(responseBody: true),
 
-        //   options.headers.addAll({
-        //     'Authorization': 'Bearer ${tokens[0]}',
-        //     'x-refresh-token': tokens[1] ?? '',
-        //   });
-        //   return handler.next(options);
-        // },
-      ),
+      InterceptorsWrapper(onRequest: (options, handler) async {
+        List<String?> tokens = await Future.wait([
+          _secureStorageService.getAccessToken(),
+          _secureStorageService.getRefreshToken(),
+          ]);
+
+          options.headers.addAll({
+            'Authorization': 'Bearer ${tokens[0]}',
+            'x-refresh-token': tokens[1] ?? '',
+          });
+          return handler.next(options);
+        }),
+
+        InterceptorsWrapper(onResponse: (response, handler) async {
+          if (response.statusCode == 401) {
+            // Handle token refresh logic here
+          }
+          List<String>? tokens = response.headers.map['x-new-tokens'];
+          if (tokens != null && tokens.length == 2) {
+            await Future.wait([
+              _secureStorageService.saveAccessToken(tokens[0]),
+              _secureStorageService.saveRefreshToken(tokens[1]),
+            ]);
+          }
+          return handler.next(response);
+
+        }),
+
+        InterceptorsWrapper(onError: (error, handler) async {
+          if (error.response?.statusCode == 401) {
+            // Handle token refresh logic here
+
+            print('Unauthorized! Redirecting to login...');
+          }
+          await Future.wait([
+            _secureStorageService.deleteAllTokens(),
+          ]);
+        })
+
+      ],
     );
   }
 
